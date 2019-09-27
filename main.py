8311b4307450bd878bbaff25c7736d0d68b81239
AnonNick <nikhilrao8@gmail.com>
2019-09-27 07:22:59 -0700

    main p2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from searchspl import searc
+import json
 inp0=input('-->')
 if inp0 == 'hello' or 'hi' or 'hey' or 'sup' or 'wasup':
 	print('Hello! Enter your query')
 	obj1= searc()
 	inp1=input('-->')
 	in1=obj1.initialsearch(inp1)
+	in1_j=json.loads(in1)
 	
