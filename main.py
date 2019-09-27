7940568ca1fa988bfa64c1d8ae8320fc612d2e57
AnonNick <nikhilrao8@gmail.com>
2019-09-27 07:17:49 -0700

    main p1

---
+++ main.py
@@ -0,0 +1,8 @@
+from searchspl import searc
+inp0=input('-->')
+if inp0 == 'hello' or 'hi' or 'hey' or 'sup' or 'wasup':
+	print('Hello! Enter your query')
+	obj1= searc()
+	inp1=input('-->')
+	in1=obj1.initialsearch(inp1)
+	
