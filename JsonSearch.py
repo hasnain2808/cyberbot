30d512697425524c1454900095c8c433a50f3ce1
hasnain2808 <hasnain2808@gmail.com>
2019-09-30 00:43:53 +0530

    fsm

--- JsonSearch.py
+++ JsonSearch.py
@@ -4,46 +4,56 @@ reference_url=[]
 reference_name=[]
 reference_source=[]
 reference_tags=[]
-with open ("/home/anonnick/CVE/nvdcve-1.1-2019.json") as datafile:
-	data = json.load(datafile)
-	CVE = raw_input("Enter CVE: ")
-	for item in data['CVE_Items']:
-		if CVE == item.get("cve").get("CVE_data_meta").get("ID"):
-			CVE_ID = item.get("cve").get("CVE_data_meta").get("ID")
-			publishDate = item.get("publishedDate")
-			problemtype = item.get("cve").get("problemtype").get("problemtype_data")[0].get("description")[0].get("value")
-			for x in range(len(item.get("cve").get("references").get("reference_data"))):
-				reference_url.insert(x,item.get("cve").get("references").get("reference_data")[x].get("url"))
-				reference_name.insert(x,item.get("cve").get("references").get("reference_data")[x].get("name"))
-				reference_source.insert(x,item.get("cve").get("references").get("reference_data")[x].get("refsource"))
-				for y in range(len(item.get("cve").get("references").get("reference_data")[x].get("tags"))):
-					reference_tags.insert(z,item.get("cve").get("references").get("reference_data")[x].get("tags")[y])
-					z = z+1
-			description_lang = item.get("cve").get("description").get("description_data")[0].get("lang")
-			description = item.get("cve").get("description").get("description_data")[0].get("value")
-			cvssv3_attackVector = item.get("impact").get("baseMetricV3").get("cvssV3").get("attackVector")
-			cvssv3_attackComplexity = item.get("impact").get("baseMetricV3").get("cvssV3").get("attackComplexity") 
-			cvssv3_privilegesRequired = item.get("impact").get("baseMetricV3").get("cvssV3").get("privilegesRequired")
-			cvssv3_userInteraction = item.get("impact").get("baseMetricV3").get("cvssV3").get("userInteraction")
-			cvssv3_confidentialityImpact = item.get("impact").get("baseMetricV3").get("cvssV3").get("confidentialityImpact")
-			cvssv3_integrityImpact = item.get("impact").get("baseMetricV3").get("cvssV3").get("integrityImpact")
-			cvssv3_availabilityImpact = item.get("impact").get("baseMetricV3").get("cvssV3").get("availabilityImpact")
-			cvssv3_baseScore = item.get("impact").get("baseMetricV3").get("cvssV3").get("baseScore")
-			cvssv3_baseSeverity = item.get("impact").get("baseMetricV3").get("cvssV3").get("baseSeverity")
-			cvssv3_exploitabilityScore = item.get("impact").get("baseMetricV3").get("exploitabilityScore")
-			cvssv3_impactScore = item.get("impact").get("baseMetricV3").get("impactScore")
-			cvssv2_accessComplexity = item.get("impact").get("baseMetricV2").get("cvssV2").get("accessComplexity")
-			cvssv2_authentication = item.get("impact").get("baseMetricV2").get("cvssV2").get("authentication")
-			cvssv2_confidentialityImpact = item.get("impact").get("baseMetricV2").get("cvssV2").get("confidentialityImpact")
-			cvssv2_integrityImpact = item.get("impact").get("baseMetricV2").get("cvssV2").get("integrityImpact")
-			cvssv2_availabilityImpact = item.get("impact").get("baseMetricV2").get("cvssV2").get("availabilityImpact")
-			cvssv2_baseScore = item.get("impact").get("baseMetricV2").get("cvssV2").get("baseScore")			
-			cvssv2_severity = item.get("impact").get("baseMetricV2").get("severity")
-			cvssv2_exploitabilityScore = item.get("impact").get("baseMetricV2").get("exploitabilityScore")
-			cvssv2_impactScore = item.get("impact").get("baseMetricV2").get("impactScore")
-			cvssv2_obtainAllPrivilege = item.get("impact").get("baseMetricV2").get("obtainAllPrivilege")
-			cvssv2_obtainUserPrivilege = item.get("impact").get("baseMetricV2").get("obtainUserPrivilege")
-			cvssv2_obtainOtherPrivilege = item.get("impact").get("baseMetricV2").get("obtainOtherPrivilege")
-			cvssv2_userInteractionRequired = item.get("impact").get("baseMetricV2").get("userInteractionRequired")
+class dessearc:
+	def __init__(self, cve):
+		self.cve =cve
+	def setattr(self):
+		with open ("nvdcve-1.1-2019.json") as datafile:
+			data = json.load(datafile)
+			# CVE = raw_input("Enter CVE: ")
+			CVE = self.cve
+			for item in data['CVE_Items']:
+				if CVE == item.get("cve").get("CVE_data_meta").get("ID"):
+					self.CVE_ID = item.get("cve").get("CVE_data_meta").get("ID")
+					self.publishDate = item.get("publishedDate")
+					self.problemtype = item.get("cve").get("problemtype").get("problemtype_data")[0].get("description")[0].get("value")
+					self.reference_url = []
+					self.reference_name = []
+					self.reference_source = []
+					self.reference_tags = []
+					for x in range(len(item.get("cve").get("references").get("reference_data"))):
+						self.reference_url.insert(x,item.get("cve").get("references").get("reference_data")[x].get("url"))
+						self.reference_name.insert(x,item.get("cve").get("references").get("reference_data")[x].get("name"))
+						self.reference_source.insert(x,item.get("cve").get("references").get("reference_data")[x].get("refsource"))
+						z = 0
+						for y in range(len(item.get("cve").get("references").get("reference_data")[x].get("tags"))):
+							self.reference_tags.insert(z,item.get("cve").get("references").get("reference_data")[x].get("tags")[y])
+							z = z+1
+					self.description_lang = item.get("cve").get("description").get("description_data")[0].get("lang")
+					self.description = item.get("cve").get("description").get("description_data")[0].get("value")
+					self.cvssv3_attackVector = item.get("impact").get("baseMetricV3").get("cvssV3").get("attackVector")
+					self.cvssv3_attackComplexity = item.get("impact").get("baseMetricV3").get("cvssV3").get("attackComplexity") 
+					self.cvssv3_privilegesRequired = item.get("impact").get("baseMetricV3").get("cvssV3").get("privilegesRequired")
+					self.cvssv3_userInteraction = item.get("impact").get("baseMetricV3").get("cvssV3").get("userInteraction")
+					self.cvssv3_confidentialityImpact = item.get("impact").get("baseMetricV3").get("cvssV3").get("confidentialityImpact")
+					self.cvssv3_integrityImpact = item.get("impact").get("baseMetricV3").get("cvssV3").get("integrityImpact")
+					self.cvssv3_availabilityImpact = item.get("impact").get("baseMetricV3").get("cvssV3").get("availabilityImpact")
+					self.cvssv3_baseScore = item.get("impact").get("baseMetricV3").get("cvssV3").get("baseScore")
+					self.cvssv3_baseSeverity = item.get("impact").get("baseMetricV3").get("cvssV3").get("baseSeverity")
+					self.cvssv3_exploitabilityScore = item.get("impact").get("baseMetricV3").get("exploitabilityScore")
+					self.cvssv3_impactScore = item.get("impact").get("baseMetricV3").get("impactScore")
+					self.cvssv2_accessComplexity = item.get("impact").get("baseMetricV2").get("cvssV2").get("accessComplexity")
+					self.cvssv2_authentication = item.get("impact").get("baseMetricV2").get("cvssV2").get("authentication")
+					self.cvssv2_confidentialityImpact = item.get("impact").get("baseMetricV2").get("cvssV2").get("confidentialityImpact")
+					self.cvssv2_integrityImpact = item.get("impact").get("baseMetricV2").get("cvssV2").get("integrityImpact")
+					self.cvssv2_availabilityImpact = item.get("impact").get("baseMetricV2").get("cvssV2").get("availabilityImpact")
+					self.cvssv2_baseScore = item.get("impact").get("baseMetricV2").get("cvssV2").get("baseScore")			
+					self.cvssv2_severity = item.get("impact").get("baseMetricV2").get("severity")
+					self.cvssv2_exploitabilityScore = item.get("impact").get("baseMetricV2").get("exploitabilityScore")
+					self.cvssv2_impactScore = item.get("impact").get("baseMetricV2").get("impactScore")
+					self.cvssv2_obtainAllPrivilege = item.get("impact").get("baseMetricV2").get("obtainAllPrivilege")
+					self.cvssv2_obtainUserPrivilege = item.get("impact").get("baseMetricV2").get("obtainUserPrivilege")
+					self.cvssv2_obtainOtherPrivilege = item.get("impact").get("baseMetricV2").get("obtainOtherPrivilege")
+					self.cvssv2_userInteractionRequired = item.get("impact").get("baseMetricV2").get("userInteractionRequired")
 
 
--- main.py
+++ main.py
@@ -1,14 +1,155 @@
 from searchspl import searc
 import json
-inp0=input('-->')
-if inp0 == 'hello' or 'hi' or 'hey' or 'sup' or 'wasup':
-	#print('Hello! Enter your query')
+from scr import CVE
+from JsonSearch import dessearc
+import requests 
+
+URLtr = "http://127.0.0.1:5000/train"
+URLpr = "http://127.0.0.1:5000/predict"
+state = "q1"
+cve = 0
+global attrlis
+
+def q1(inp):
+	if inp0 == 'hello' or 'hi' or 'hey' or 'sup' or 'wasup':
+		global state 
+		state = 'q2'
+		# print(state)
+		print("q1")
+		return "hey whats your domain ?"
+	else :
+		return "I wasn't able to understand please reframe"
+
+def q2(inp):
 	obj1= searc()
 	#inp1=input('-->')
-	in1=str(obj1.initialsearch("afd windows local")[0])[1:]
-#	print(in1[0])
+	#"afd windows local"
+	print("q2")
+	in1=str(obj1.initialsearch(inp)[0])[1:]
 	in1 = in1.replace('\\n','').replace('\\t','').replace('\\','')[1:-1]
-#	print(in1)
 	in1_j=json.loads(in1)
-	print(in1_j["SEARCH"])
-#	print(in1_j)
+	global state 
+	state = "q3"
+	return in1
+
+def q3(inp):
+	print("q3")
+	ebd = CVE(inp)
+	cve = ebd.find()
+	global state
+	state = "q4"
+	global attrlis
+	# attrlis = dessearc('CVE-'+str(cve))
+	# 2019-0013
+	attrlis = dessearc('CVE-'+str("2019-0013"))
+	attrlis.setattr()
+	return cve
+
+def q4(inp):
+	print("q4")
+	global state
+	global attrlis
+
+	if inp == "qa":
+		desc = attrlis.description + ',' + attrlis.description
+		PARAMS = {'tr':desc} 
+		requests.get(url = URLtr, params = PARAMS) 
+		state = "q5"
+		return "transfer learning..."
+	# state = "q5"
+	if inp == "publishDate":
+		return attrlis.publishDate
+	if inp == "problemtype":
+		return attrlis.problemtype
+	if inp == "reference_url":
+		return attrlis.reference_url
+	if inp == "reference_name":
+		return attrlis.reference_name
+	if inp == "reference_source":
+		return attrlis.reference_source
+	if inp == "reference_tags":
+		return attrlis.reference_tags 
+	if inp == "description_lang":
+		return attrlis.description_lang
+	if inp == "description":
+		return attrlis.description
+	if inp == "cvssv3_attackVector":
+		return attrlis.cvssv3_attackVector
+	if inp == "cvssv3_attackComplexity":
+		return attrlis.cvssv3_attackComplexity
+	if inp == "cvssv3_privilegesRequired":
+		return attrlis.cvssv3_privilegesRequired
+	if inp == "cvssv3_userInteraction":
+		return attrlis.cvssv3_userInteraction
+	if inp == "cvssv3_confidentialityImpact":
+		return attrlis.cvssv3_confidentialityImpact
+	if inp == "cvssv3_integrityImpact":
+		return attrlis.cvssv3_integrityImpact
+	if inp == "cvssv3_availabilityImpact":
+		return attrlis.cvssv3_availabilityImpact
+	if inp == "cvssv3_baseScore":
+		return attrlis.cvssv3_baseScore
+	if inp == "cvssv3_baseSeverity":
+		return attrlis.cvssv3_baseSeverity
+	if inp == "cvssv3_exploitabilityScore":
+		return attrlis.cvssv3_exploitabilityScore
+	if inp == "cvssv3_impactScore":
+		return attrlis.cvssv3_impactScore
+	if inp == "cvssv2_accessComplexity":
+		return attrlis.cvssv2_accessComplexity
+	if inp == "cvssv2_authentication":
+		return attrlis.cvssv2_authentication
+	if inp == "cvssv2_confidentialityImpact":
+		return attrlis.cvssv2_confidentialityImpact
+	if inp == "cvssv2_integrityImpact":
+		return attrlis.cvssv2_integrityImpact
+	if inp == "cvssv2_availabilityImpact":
+		return attrlis.cvssv2_availabilityImpact
+	if inp == "cvssv2_baseScore":
+		return attrlis.cvssv2_baseScore
+	if inp == "cvssv2_severity":
+		return attrlis.cvssv2_severity
+	if inp == "cvssv2_exploitabilityScore":
+		return attrlis.cvssv2_exploitabilityScore
+	if inp == "cvssv2_impactScore":
+		return attrlis.cvssv2_impactScore
+	if inp == "cvssv2_obtainAllPrivilege":
+		return attrlis.cvssv2_obtainAllPrivilege
+	if inp == "cvssv2_obtainUserPrivilege":
+		return attrlis.cvssv2_obtainUserPrivilege
+	if inp == "cvssv2_obtainOtherPrivilege":
+		return attrlis.cvssv2_obtainOtherPrivilege
+	if inp == "cvssv2_userInteractionRequired":
+		return attrlis.cvssv2_userInteractionRequired
+	return "attribute not found"
+
+def q5(inp):
+	if inp == "bye":
+		state = "q9"
+		return "Bye"
+	PARAMS = {'tr':inp} 
+	rr = requests.get(url = URLpr, params = PARAMS) 
+	return rr.content
+
+
+while True:
+	# global state
+	inp0=input('-->')
+	print("sta ",state)
+	if state == "q1":
+		out0 = q1(inp0)
+		print(out0)
+	elif state == "q2":
+		out0 = q2(inp0)
+		print(out0)
+	elif state == "q3":
+		out0 = q3(inp0)
+		print("enter what attribute you want, enter qa to enter qa mode")
+	elif state == "q4":
+		out0 = q4(inp0)
+		print(out0)
+	elif state == "q5":
+		out0 = q5(inp0)
+		print(out0)
+	else:
+		print("thanks and bbye")
